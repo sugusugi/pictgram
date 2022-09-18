db01d4414b66b511bc1152ce6dc5cc8c79c1b760
sugi <pokemon50babakun@ezweb.ne.jp>
2022-09-18 14:21:53 +0900

    add 27. S3 への画像のアップロード/first

--- src/main/java/com/example/pictgram/AppConfig.java
+++ src/main/java/com/example/pictgram/AppConfig.java
@@ -3,8 +3,11 @@ package com.example.pictgram;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {ContextInstanceDataAutoConfiguration.class})
 public class AppConfig {
     
     @Bean
